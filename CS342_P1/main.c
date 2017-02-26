54728b79fda9ab9915511b920724a99026ed0ee3
Çelik Köseoğlu <celikkoseoglu@Celiks-MacBook-Pro.local>
2017-02-26 19:31:31 +0300

    now to merge all intermediary files when children exit

--- CS342_P1/main.c
+++ CS342_P1/main.c
@@ -1,51 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char *argv) {
-
-    //argv[0] is the name of the program  3 in1.txt in2.txt in3.txt out.txt
-
-    printf("%s\n", argv[1]);
-
-    /*printf("%s\n", argv[2]);
-    printf("%s\n", argv[3]);
-    printf("%s\n", argv[4]);
-
-
-    const int numberOfInputs = atoi(argv[2]);
-
-    for (int i = 3; i < numberOfInputs + 3; i++) {
-        printf(argv[i]);
-    }
-
-    const char *const outputFileName = argv[numberOfInputs + 4];
-
-
-    printf("Hello, World!\n");*/
-    return 0;
-}
-
-/*void readFile(char *inputFileName) {
+void processFile(char *inputFileName, char *outputFileName, const char *keyword) {
 
     //file reading part is taken from the C language library guides. The following is possibly the best way to do it since you don't have to allocate a pre-determined size for the input buffer
-    FILE * fp;
-    char * line = NULL;
+    FILE *fr, *fw;
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
-    fp = fopen(inputFileName, "r");
-    if (fp == NULL)
+    fr = fopen(inputFileName, "r");
+    fw = fopen(outputFileName, "w+");
+
+    if (fr == NULL)
         exit(EXIT_FAILURE);
 
-    while ((read = getline(&line, &len, fp)) != -1) {
+    int lineNumber = 1;
+
+    while ((read = getline(&line, &len, fr)) != -1) {
 
         if( line[read-1] == '\n' ) //replace line endings with string terminators. Well, maybe this results in a memory leak because "someString\n\0" becomes "someString\0\0". Not sure about that though.
             line[read-1] = '\0';
 
-        printf("%s\n", line);
+        char *line_copy;
+        line_copy = (char*)malloc(sizeof(char) * strlen(line));
+        strcpy(line_copy, line);
+
+        char *tok = line, *end = line;
+        while (tok != NULL) {
+            strsep(&end, " ");
+
+            if (strcmp(keyword, tok) == 0) {
+                fprintf(fw, "%s, %d: %s\n", inputFileName, lineNumber, line_copy);
+                break;
+            }
+
+            tok = end;
+        }
+
+        lineNumber++;
     }
 
-    //fp = fopen(outputFileName, "w+");
+    fclose(fw);
 
-    fclose(fp);
+}
 
-}*/
+int main(int argc, char **argv) {
+
+    //argv[0] is the name of the program hello 3 in1.txt in2.txt in3.txt out.txt
+
+    const char *const keyword = argv[1];
+
+    printf("\nKeyword to search for: %s", keyword);
+
+    const int numberOfInputs = atoi(argv[2]);
+
+    //printf("\nNumber of Inputs: %d", numberOfInputs);
+
+
+    for (int i = 3; i < numberOfInputs + 3; i++) {
+
+        pid_t childProcessId = fork();
+
+        if (childProcessId < 0) {
+            printf("\nCannot create child process. Exiting...");
+            exit(1);
+        } else if (childProcessId == 0) {
+            printf("\nChild (%d): %d\n", i + 1, getpid());
+            printf("\nInput file name: %s", argv[i]);
+
+            char str[10];
+            sprintf(str, "%d.txt", i);
+
+            processFile(argv[i], str, keyword);
+
+            exit(0);
+        }
+
+    }
+
+    /*if (child == 0)
+        readFile(argv[3]);
+    else
+        readFile(argv[4]);*/
+
+
+    /*const char *const outputFileName = argv[numberOfInputs + 3];
+
+    printf("\nOutput file name:  %s", outputFileName);*/
+
+    return 0;
+}
